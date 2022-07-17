297e860ace383b4b3b8aa8cb3f84242abd276764
momin-sarker <mominsarker21@gmail.com>
2022-07-17 05:21:25 -0700

    1st owl carousel commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        margin:15,
+        items:3,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            1000:{
+                items:2
+            },
+            1200:{
+                items:3
+            },
+            1900:{
+                items:4
+            }
+        }
+
+    });
+  });
